78713d8957261e12ed06014c49ecd192d06082b4
Cassiano Borba <bcassiano888@gmail.com>
2025-10-20 14:22:12 -0300

    Update CPC.py
    adiconado formatador

--- CPC.py
+++ CPC.py
@@ -1,5 +1,6 @@
 def tokenizador(f):
     import nltk
+    nltk.download('punkt_tab')
 
     from nltk.tokenize import word_tokenize
 
@@ -11,9 +12,9 @@ def subOperador(f):
     operadores = {
         'e': '^',
         'ou': 'v',
-        'se': '-->',
-        'então': '-->',
-        'entao': '-->',
+        'se': '⭢',
+        'então': '⭢',
+        'entao': '⭢',
         'não': '~',
         'nao': '~'
     }
@@ -26,18 +27,36 @@ def subOperador(f):
             frase1.append(t)
     frasef = ' '.join(frase1)
     return frasef
+a = 'eu nao vou a praia e amanha chove'
 
+def formato(f):
+  import re
+  frase = subOperador(f)
+  blocos = re.split(r'\s*(\^|⭢|\bv\b)\s*', frase)
+  cap = []
+  for i in blocos:
+    elemento = i.split()
+    for j in elemento:
+      passou = False
+      if j == "~":
+        cap.append(j+(elemento[-1][0]))
+        passou = True
+      if j in ['^','v','⭢']:
+        cap.append(j)
+        passou = True
+      
+      
+  return cap
+
+print(formato(a))
 def treinotag():
     import nltk
     from nltk.tag import UnigramTagger
     from nltk.corpus import mac_morpho
-    train_data = mac_morpho.tagged_sents()[:5000]  # 3000 sentenças do Mac-Morpho
+    train_data = mac_morpho.tagged_sents()[:8000]
     tagger = UnigramTagger(train_data)
-    frase = "O rápido gato preto pula alegremente sobre a mesa e olha para o cachorro que dorme." 
+    frase = "O rápido gato preto pula alegremente sobre a mesa e olha para o cachorro que dorme."
     a = tokenizador(frase)
     tags = tagger.tag(a)
     print(tags)
-
-
 treinotag()
-
